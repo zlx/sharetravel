6e05cea015c6c578e9c769566d687ffd8cb160aa
soffolk zhu <zlx.star@gmail.com>
2012-11-03 13:07:18 -0400

    init sharetravel app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sharetravel::Application.config.secret_token = 'b1700cffba8c5d3df1f8458d77ed9ddab522dac788e0b307f47c93552d8a95ac1051871186e417c2aa663660c3f014c727cd506bd2c603cc401d32292a521c82'
